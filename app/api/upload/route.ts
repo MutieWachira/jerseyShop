b23471e3a5bfe782c8e8aa30779052c9852974e9
MutieWachira <151459388+MutieWachira@users.noreply.github.com>
2026-07-09 18:12:29 +0300

    Fixed a bug crash where when uploading an image caught an error since we were writing to a local file. The updated code uploads an image to the amazon S3 bucket for storage.

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,30 +1,37 @@
-import { NextResponse } from "next/server"
-import fs from "fs"
-import path from "path"
+import { S3Client, PutObjectCommand } from  "@aws-sdk/client-s3";
+import { randomUUID } from "crypto";
 
-export async function POST(req: Request){
+const s3 = new S3Client ({
+  region: process.env.AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 
-  const formData = await req.formData()
-  const file = formData.get("file") as File
+export async function POST(req:Request){
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
 
-  if(!file){
-    return NextResponse.json({error:"No file uploaded"})
+  if (!file) {
+    return Response.json({error: "No file uploaded"}, { status: 400 });
   }
 
-  const bytes = await file.arrayBuffer()
-  const buffer = Buffer.from(bytes)
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+  const key = `product/${randomUUID()}-${file.name}`;
 
-  const uploadDir = path.join(process.cwd(),"public/uploads")
-
-  if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir,{recursive:true})
-  }
-
-  const filePath = path.join(uploadDir,file.name)
-
-  fs.writeFileSync(filePath,buffer)
-
-  return NextResponse.json({
-    path:`/uploads/${file.name}`
-  })
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: process.env. AWS_S3_BUCKET_NAME!,
+      Key: key,
+      Body: buffer,
+      ContentType: file.type,
+    })
+  );
+  return Response.json({
+    path: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`,
+  });
 }
+
+
